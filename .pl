03d8115ce0628d4cd4e2f13ccca0b5056e411323
Interface Guy <david.rug98@icloud.com>
2024-08-20 14:21:46 +0100

    initial commit

---
+++ .pl
@@ -0,0 +1,6 @@
+{
+  "dateCreated": "2024-08-15T18:24:25.255Z",
+  "dateModified": "2024-08-15T18:24:25.255Z",
+  "interactions": 0,
+  "type": "idea"
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# DreamNode
+# DownwardSpiralTowardsHell
+![](DownwardSpiralTowardsHell.png)
